2313c79a3bff7eeba34f2589a9f8afa860134440
Leonardo Viana <leonardoviana00@hotmail.com>
2023-02-10 23:47:04 -0300

    feat(argocd): application

--- app/apply.sh
+++ app/apply.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# Criando namespaces
+# Criando namespaces e injetando o istio
+
+# Ambiente de desenvolvimento
 # kubectl create namespace demo-app-dev
 # kubectl label namespace demo-app-dev istio-injection=enabled
 
+# Ambiente de homologação
 # kubectl create namespace demo-app-hom
 # kubectl label namespace demo-app-hom istio-injection=enabled
 
+# Ambiente de produção
 # kubectl create namespace demo-app-prod
 # kubectl label namespace demo-app-prod istio-injection=enabled
 
 # Aplicando os deployments multiplos ambientes
-kubectl apply -k overlays/development/
-kubectl apply -k overlays/staging/
-kubectl apply -k overlays/production/
+# kubectl apply -k overlays/development/
+# kubectl apply -k overlays/staging/
+# kubectl apply -k overlays/production/
 
-# kubectl delete -k overlays/development/
+# Deletando ambientes
+kubectl delete -k overlays/development/
+kubectl delete -k overlays/staging/
+kubectl delete -k overlays/production/
