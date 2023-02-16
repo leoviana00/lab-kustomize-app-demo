dce79d330378c8435ad48e7a0313ab421cb68194
Leonardo Viana <leonardoviana00@hotmail.com>
2023-02-16 05:26:51 -0300

    feature(argocd-kustomize): fix privat ip etc/hosts

--- scripts/local_dns.sh
+++ scripts/local_dns.sh
@@ -9,16 +9,16 @@ config_hosts(){
 echo "Setting DNS Local /etc/hosts"
 cat >>/etc/hosts<<EOF
 # Lab Kustomize - DNS Local para aplicacao demo nginx e go
-192.168.1.11   squad.a.nginx.lab.k8s.io
-192.168.1.11   ws.dev.squad.a.go.lab.k8s.io
-192.168.1.11   ws.hom.squad.a.go.lab.k8s.io
-192.168.1.11   ws.squad.a.go.lab.k8s.io
+10.0.2.206   squad.a.nginx.lab.k8s.io
+10.0.2.206   ws.dev.squad.a.go.lab.k8s.io
+10.0.2.206   ws.hom.squad.a.go.lab.k8s.io
+10.0.2.206   ws.squad.a.go.lab.k8s.io
 
 # Balanceador - DNS Local para o lb
-192.168.1.11   lb.lab.k8s.io 
+10.0.2.206   lb.lab.k8s.io 
 
 # Argocd - DNS local para o argocd
-192.168.1.11   argocd.lab.k8s.io
+10.0.2.206   argocd.lab.k8s.io
 EOF
 }
 
